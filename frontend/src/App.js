71ab896e9a6ccb986f07dce94db8741a0b95a557
devloperdeepanshu <153080609+devloperdeepanshu@users.noreply.github.com>
2025-04-21 14:25:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
     };
 
     try {
-      const res = await fetch('http://localhost:5000/api/register', {
+      const res = await fetch('https://insta-repo-2.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user),
